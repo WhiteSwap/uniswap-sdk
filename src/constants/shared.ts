bb1ea3762d2f9b147c9bf6b8ef1c8ad38fa5997d
Natalia Gorbova <natalia.gorbova@whitebit.com>
2022-09-19 12:57:02 +0200

    feat: added factory addresses for all chains

--- src/constants/shared.ts
+++ src/constants/shared.ts
@@ -3,9 +3,9 @@ import { ChainId, SolidityType } from '../types'
 
 export const FACTORY_ADDRESS: { [key in ChainId]: string } = {
   [ChainId.MAINNET]: '0x69bd16aE6F507bd3Fc9eCC984d50b04F029EF677',
-  [ChainId.GOERLI]: '0x69bd16aE6F507bd3Fc9eCC984d50b04F029EF677',
-  [ChainId.POLYGON]: '',
-  [ChainId.POLYGON_MUMBAI]: '0xf9d191e14982441e045f13e94f78096be01f6421'
+  [ChainId.GOERLI]: '0x3f0b5743bba8a552a3aa1e7907f4f44047e93f8f',
+  [ChainId.POLYGON]: '0xDaFb251f372D040F0DA0f9B787623aAE9cF1ACB6',
+  [ChainId.POLYGON_MUMBAI]: '0x3f0b5743bba8a552a3aa1e7907f4f44047e93f8f'
 }
 
 export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
