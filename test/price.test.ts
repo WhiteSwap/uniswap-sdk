67b99f8e8d084889a00aa214e32a523dc256d418
oleksii.oleksii <oleksii.oleksii@whitebit.com>
2022-12-14 13:06:12 +0100

    fix(Price): add base currency decimal places by default to toDecimalPlaces method. create toExact method

--- test/price.test.ts
+++ test/price.test.ts
@@ -1,17 +1,36 @@
 import JSBI from 'jsbi'
-import { ChainId, Price, Token } from '../src'
+import { ChainId, Price, Token, WRAPPED_NATIVE_CURRENCY } from '../src'
 
 describe('Price', () => {
   const USDC = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 18, 'USDC', 'USD Coin')
   const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'DAI Stablecoin')
-  const amount = JSBI.BigInt(1234)
+  const USDT = new Token(ChainId.MAINNET, '0xdac17f958d2ee523a2206206994597c13d831ec7', 6, 'USDT', 'Tether')
+  const daiUsdcPrice = new Price(DAI, USDC, JSBI.BigInt(1000), JSBI.BigInt(1234))
+  const usdtWethPrice = new Price(
+    USDT,
+    WRAPPED_NATIVE_CURRENCY[ChainId.MAINNET],
+    JSBI.BigInt(1000000000),
+    JSBI.BigInt(1234567890123456789122)
+  )
 
   describe('#toDecimalPlaces', () => {
     it('should round price to default decimals', () => {
-      expect(new Price(DAI, USDC, JSBI.BigInt(1000), amount).toDecimalPlaces()).toEqual('1.234')
+      expect(daiUsdcPrice.toDecimalPlaces()).toEqual('1.234')
     })
     it('should round price to specified decimals', () => {
-      expect(new Price(DAI, USDC, JSBI.BigInt(1000), amount).toDecimalPlaces(2)).toEqual('1.23')
+      expect(daiUsdcPrice.toDecimalPlaces(2)).toEqual('1.23')
+    })
+    it('should round price to quotient currency', () => {
+      expect(usdtWethPrice.toDecimalPlaces()).toEqual('1.234568')
+    })
+    it('should round price to inverted quotient currency', () => {
+      expect(usdtWethPrice.invert().toDecimalPlaces()).toEqual('0.81000001')
+    })
+    it('should return exact price to inverted quotient currency', () => {
+      expect(usdtWethPrice.invert().toExact()).toEqual('0.810000007290000076')
+    })
+    it('should return exact price to quotient currency', () => {
+      expect(usdtWethPrice.toExact()).toEqual('1.234568')
     })
   })
 })
