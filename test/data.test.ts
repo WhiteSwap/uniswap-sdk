0159af1a7053554d42a874d82c19742cce97a7f9
oleksii.oleksii <oleksii.oleksii@whitebit.com>
2023-01-17 14:40:27 +0100

    test(SDK): restore data fetcher unit tests

--- test/data.test.ts
+++ test/data.test.ts
@@ -1,20 +1,33 @@
-import { ChainId, Token, Fetcher, WRAPPED_NATIVE_CURRENCY } from '../src'
+import { ChainId, Token, Fetcher } from '../src'
 
 // TODO: replace the provider in these tests
-describe.skip('data', () => {
+describe('data', () => {
   it('Token', async () => {
-    const token = await Fetcher.fetchTokenData(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F') // DAI
+    const token = await Fetcher.fetchTokenData(
+      ChainId.MAINNET,
+      '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+      undefined,
+      'DAI',
+      'DAI Stable Coin'
+    ) // DAI
     expect(token.decimals).toEqual(18)
   })
 
   it('Token:CACHE', async () => {
-    const token = await Fetcher.fetchTokenData(ChainId.MAINNET, '0xE0B7927c4aF23765Cb51314A0E0521A9645F0E2A') // DGD
+    const token = await Fetcher.fetchTokenData(
+      ChainId.MAINNET,
+      '0xE0B7927c4aF23765Cb51314A0E0521A9645F0E2A',
+      undefined,
+      'DGD',
+      'DGD Stable Coin'
+    ) // DGD
     expect(token.decimals).toEqual(9)
   })
 
   it('Pair', async () => {
-    const token = new Token(ChainId.GOERLI, '0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735', 18) // DAI
-    const pair = await Fetcher.fetchPairData(WRAPPED_NATIVE_CURRENCY[ChainId.GOERLI], token)
-    expect(pair.liquidityToken.address).toEqual('0x8B22F85d0c844Cf793690F6D9DFE9F11Ddb35449')
+    const TWSD = new Token(ChainId.GOERLI, '0xce1689b266373D78f9DfbD9e4889baEdD8751EB8', 18, 'TWSD', 'TWSD') // DAI
+    const TUSDT = new Token(ChainId.GOERLI, '0x0239523c6ed5614fbd16020423097d6073a06c85', 6, 'TUSDT', 'TUSDT') // DAI
+    const pair = await Fetcher.fetchPairData(TWSD, TUSDT)
+    expect(pair.liquidityToken.address).toEqual('0xeAcE72edf6B9f5d872A8c40A8e8997aB13C9bb66')
   })
 })
